6dfac46824821714e6f3d79d8d1597fa71287124
Evgeniy Baksanov <baksanovv1@gmail.com>
2026-08-31 12:39:12 +0000

    complete docker practice homework

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+REPO_URL="<https://github.com/baksanovev/shvirtd-example-python>"
+APP_DIR="/opt/shvirtd-example-python"
+
+echo "=== Deploy shvirtd project ==="
+
+if [ -d "$APP_DIR/.git" ]; then
+    echo "Repository already exists. Updating..."
+    git -C "$APP_DIR" pull --ff-only
+else
+    echo "Cloning repository..."
+    git clone "$REPO_URL" "$APP_DIR"
+fi
+
+cd "$APP_DIR"
+
+echo "Starting containers..."
+
+docker compose up -d --build
+
+echo "Containers:"
+docker compose ps
